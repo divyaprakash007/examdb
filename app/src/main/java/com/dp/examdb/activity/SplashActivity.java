03a57b8bafe91794642ae23bb6ccc4668870edc1
DIVYA PRAKASH <Sunaina@123>
2021-04-22 16:22:25 +0530

    Alarm Clock Testing

--- app/src/main/java/com/dp/examdb/activity/SplashActivity.java
+++ app/src/main/java/com/dp/examdb/activity/SplashActivity.java
@@ -16,7 +16,7 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        if (AppUtils.isNetworkAvailable(this)) {
+        if (!AppUtils.isNetworkAvailable(this)) {
             startActivity(new Intent(this, NoInternetConnection.class));
         } else {
             new Handler().postDelayed(new Runnable() {
